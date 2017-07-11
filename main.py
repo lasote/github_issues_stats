739542ba24884d73e2cd51922474e9216a8ab740
Luis Martinez de Bartolome <lasote@gmail.com>
2017-07-11 12:51:32 +0300

    refactor

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 # coding=utf-8
+import csv
 import datetime
 import os
-
 from collections import namedtuple
 
+import dateutil.parser
 import requests
 from dateutil.relativedelta import relativedelta
 
 repo = "conan-io/conan"
 username = "lasote"
 token = os.getenv("GH_TOKEN", "")
+csv_filename  = "issues.csv"
+
+init_date = datetime.date(2017, 1, 1)
+end_date = datetime.date(2017, 7, 31)
+delta = relativedelta(months=1)
 
 
 def get_issues(from_date, repo, user, key):
     Issue = namedtuple("issue", "created_at, state, comments")
     ret = {}
     iso_since = from_date.isoformat()
-    for page in range(100):
+    for page in range(1000):
         issues = requests.get("https://api.github.com/repos/%s/issues"
                               "?page=%d&since=%s&state=all" % (repo, page, iso_since), auth=(user, key))
         data = issues.json()
@@ -28,8 +34,6 @@ def get_issues(from_date, repo, user, key):
 
 
 def write_cvs(rows, filename):
-    import csv
-    import os
 
     if os.path.exists(filename):
         os.unlink(filename)
@@ -46,28 +50,19 @@ def get_rows(init_date, end_date, repo, user, key):
     rows = []
     while tmp < end_date:
         tmp = tmp + delta
-        num_open_issues = 0
-        num_closed_issues = 0
-        num_comments = 0
+        num_open_issues = num_closed_issues = num_comments = 0
         for issue_id, issue in all_issues.items():
-            import dateutil.parser
             created_at = dateutil.parser.parse(issue.created_at)
-            if created_at.date() < (tmp + delta) and created_at.date() >= tmp:
+            if tmp >= created_at.date() < (tmp + delta):
                 num_open_issues += 1 if issue.state == "open" else 0
                 num_closed_issues += 1 if issue.state == "closed" else 0
                 num_comments += issue.comments
 
         row = [tmp.isoformat(), str(num_open_issues), str(num_closed_issues), str(num_comments)]
         rows.append(row)
-        print(row)
     return rows
 
 if __name__ == "__main__":
-    from datetime import timedelta
-    init_date = datetime.date(2017, 1, 1)
-    end_date = datetime.date(2017, 7, 31)
-    delta = relativedelta(months=1)
-
     rows = get_rows(init_date, end_date, repo, username, token)
-
-    write_cvs(rows, 'issues.csv')
+    print(rows)
+    write_cvs(rows, csv_filename)
